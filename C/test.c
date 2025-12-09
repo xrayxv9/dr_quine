2184b720ca93b85c80943ac025ad060ba75c1fab
xray <morel.clement@proton.me>
2025-12-09 14:10:43 +0100

    C part finished

--- C/test.c
+++
@@ -1,2 +0,0 @@
-#include <stdio.h>
-/*My beautiful foo function*/int foo(){return 1;}int main(){char *str = "#include <stdio.h>%c/*My beautiful foo function*/int foo(){return 1;}int main(){char *str = %c%s%c;/*called the foo function*/foo();printf(str, 10, 34, str, 34, 10);}%c";/*called the foo function*/foo();printf(str, 10, 34, str, 34, 10);}
