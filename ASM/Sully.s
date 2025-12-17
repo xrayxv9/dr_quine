8261b233199cccbb2c372288cca033a4f0a100c1
Clement Morel <cmorel@3G3.42angouleme.fr>
2025-12-17 20:16:24 +0100

    flemme d'oublie

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,23 +1,21 @@
+%define	O_WRONLY	1
+%define	O_CREAT		0x40
+%define	O_TRUNC		0x200
+%define	X			5
+
 section	.data
-	filename:		db "Sully_%d.s", 0
-	command:		db "nasm -f elf64 Sully_%1$d.s; gcc Sully_%1$d.o; /a.out", 0
-	str:			db "section	.data%1$c%2$cfilename:		db %4$cSully_%5$cd.s, 0%1$c%2$ccommand: db		%4$ccc %5$cs; ./a.out%4$c, 0%1$c%2$cstr: db			%4$c%3$c%4$c%1$c%2$cfilename_size	equ 24%1$c%2$ccommand_size	equ 24%1$c%1$csection .text%1$c%1$cextern	sprintf%1$cextern	dprintf%1$cextern	close%1$cextern	exit%1$cextern	system%1$cglobal	main%1$c%5$cdefine O_WRONLY	1%1$c%5$cdefine O_CREAT		0x40%1$c%5$cdefine O_TRUNC		0x200%1$c%1$cmain:%1$c%2$cpush	rbp%1$c%2$cmov		rbp, rsp%1$c%1$c%2$cpush r12%1$c%2$cpush	r13%1$c%2$cpush	r14%1$c%1$c%2$cmov		r14, %6$d%1$c%2$ccmp		r14, -1%1$c%2$cjle		.exit%1$c%2$csub		rsp, filename_size%1$c%2$clea		rdi, [rsp]%1$c%2$clea		rsi, [rel filename]%1$c%2$cmov		rdx, r14%1$c%2$cxor		rax, rax%1$c%2$ccall	sprintf wrt ..plt%1$c%2$csub		rdi, 9%1$c%2$cmov		r12, rdi%1$c%2$cmov		rsi, O_CREAT | O_WRONLY | O_TRUNC%1$c%2$cmov		rcx, 0644o%1$c%2$ccall	open wrt ..plt%1$c%2$ccmp		rax, -1%1$c%2$cje		.exit_file%1$c%2$cmov		r13, rax%1$c%2$cmov		rdi, rax%1$c%2$clea		rsi, [rel str]%1$c%2$cmov		rdx, 10%1$c%2$cmov		rcx, 9%1$c%2$cmov		r9, 34%1$c%2$cpush	37%1$c%2$csub		r14, 1%1$c%2$cpush	r14%1$c%2$cmov		rax, 2%1$c%2$ccall	dprintf wrt ..plt%1$c%2$csub		rsp, command_size%1$c%2$clea		rdi, [rsp]%1$c%2$clea		rsi, [rel command]%1$c%2$cmov		rdx, r12%1$c%2$cxor		rax, rax%1$c%2$ccall	sprintf wrt ..plt%1$c%2$cadd		rsi, command_size%1$c%2$ccall	system wrt ..plt%1$c%2$cmov		rdi, r12%1$c%2$ccall	close wrt ..plt%1$c.exit_file:%1$c%2$cadd		rsp, filename_size%1$c.exit:%1$c%2$cpop		r14%1$c%2$cpop		r13%1$c%2$cpop		r12%1$c%2$cpop		rbp%1$c%2$cret"
-	filename_size	equ 32
-	command_size	equ 64
+	filename:		db "Sully_", X + '0', ".s", 0
+	command:		db "nasm -f elf64 Sully_", X + '0', ".s; gcc Sully_", X + '0', ".o; /a.out", 0
+	str:			db "%5$cdefine	O_WRONLY	1%1$c%5$cdefine	O_CREAT		0x40%1$c%5$c	O_TRUNC		0x200%1$c%5$cdefine	X			%6$c%1$c%1$csection	.data%1$c%2$cfilename:		db %4$cSully_%4$c, X + '0', %4$c.s%4$c, 0%1$c%2$ccommand:		db %4$cnasm -f elf64 Sully_%4$c, X + '0', %4$c.s; gcc Sully_%4$c, X + '0', %4$c.o; ./a.out%4$c, 0%1$c%2$cstr:			%4$c%3$c%4$c%1$c%1$csection	%1$c%1$c%2$cextern	dprintf%1$cextern	open%1$cextern	close%1$cextern	exit%1$cextern	system%1$cglobal	main%1$c%1$cmain:%1$c%2$cpush	rbp%1$c%2$cmov		rbp, rsp%1$c%1$c%2$cpush	r13%1$c%2$cpush	r14%1$c%1$c%2$cmov		r14, X%1$c%2$ccmp		r14, -1%1$c%2$cjle		.exit%1$c%2$clea		[rdi, rel filename]%1$c%2$cmov		esi, O_CREAT | O_WRONLY | O_TRUNC%1$c%2$cmov		rdx, 0644o%1$c%2$cxor		rax, rax%1$c%2$ccall	open wrt ..plt%1$c%2$ccmp		rax, -1%1$c%2$cje		.exit%1$c%2$cmov		r13, rax%1$c%2$cmov		rdi, rax%1$c%2$clea		rsi, [rel str]%1$c%2$cmov		rdx, 10%1$c%2$cmov		rcx, 9%1$c%2$clea		r8, [rel str]%1$c%2$cmov		r9, 34%1$c%2$cpush	37%1$c%2$csub		r14, 1%1$c%2$cpush	r14%1$c%2$cmov		rax, 2%1$c%2$ccall	dprintf wrt ..plt%1$c%2$clea		rdi, [rel command]%1$c%2$ccall	system wrt ..plt%1$c%2$cmov		rdi, r13%1$c%2$csub		rsi, 8%1$c%2$ccall	close wrt ..plt%1$c%2$cadd		rsi, 8%1$c.exit:%1$c%2$cpop		r14%1$c%2$cpop		r13%1$c%2$cpop		rbp%1$c%2$cret"
 
-section .text
+section	.text
 
-extern	sprintf
 extern	dprintf
 extern	open
 extern	close
 extern	exit
 extern	system
 global	main
-
-%define O_WRONLY	1
-%define O_CREAT		0x40
-%define O_TRUNC		0x200
 
 main:
 	push	rbp
@@ -26,22 +24,16 @@ main:
 	push	r13
 	push	r14
 
-	mov		r14, 5
+	mov		r14, X
 	cmp		r14, -1
 	jle		.exit
-	sub		rsp, filename_size
-	lea		rdi, [rsp]
-	lea		rsi, [rel filename]
-	mov		rdx, r14
-	xor		rax, rax
-	call	sprintf wrt ..plt
-	sub		rdi, rax
+	lea		rdi, [rel filename]
 	mov		esi, O_CREAT | O_WRONLY | O_TRUNC 
 	mov		rdx, 0644o
-	xor		rax,rax
+	xor		rax, rax
 	call	open wrt ..plt
 	cmp		rax, -1
-	je		.exit_file
+	je		.exit
 	mov		r13, rax
 	mov		rdi, rax
 	lea		rsi, [rel str]
@@ -54,21 +46,12 @@ main:
 	push	r14
 	mov		rax, 2
 	call	dprintf wrt ..plt
-	sub		rsp, command_size
-	lea		rdi, [rsp]
-	lea		rsi, [rel command]
-	mov		rdx, r14
-	xor		rax, rax
-	call	sprintf wrt ..plt
-	sub		rdi, rax
-	add		rsi, command_size
+	lea		rdi, [rel command]
 	call	system wrt ..plt
 	mov		rdi, r13
 	sub		rsi, 8
 	call	close wrt ..plt
 	add		rsi, 8
-.exit_file:
-	add		rsp, filename_size
 .exit:
 	pop		r14
 	pop		r13
